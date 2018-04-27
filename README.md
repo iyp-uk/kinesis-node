4d28fed06ef978c7d1c4625ec3b7d571ead81189
Jerome Krieg <miaoulafrite@gmail.com>
2018-04-27 18:20:58 +0100

    Add AWS Kinesis. Stop server if unable to connect to AWS.

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ It exposes allows POST requests on its root endpoint (`/`) and sends the payload
 ## Health check
 
 There is also a basic ping / pong healthcheck available at `/ping` and it replies with `{"message": "pong"}`.
+
+## Connection with AWS
+
+You **must** specify the following environment variables:
+
+- AWS_ACCESS_KEY_ID
+- AWS_SECRET_ACCESS_KEY
+
+You **can** also specify:
+
+- `AWS_REGION`, will default to `eu-west-1`
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var createError = require('http-errors')
+var express = require('express')
+var router = express.Router()
 
-router.get('/', function(req, res, next) {
-  res.json({ message: 'ok' });
-});
+var AWS_REGION = process.env.AWS_REGION || 'eu-west-1'
+var STREAM_NAME = process.env.STREAM_NAME || 'kafka-vs-kinesis'
 
-module.exports = router;
+var AWS = require('aws-sdk')
+var kinesis = new AWS.Kinesis({region: AWS_REGION})
+var witeToKinesis = function (streamName, data) {
+  var record = {
+    Data: data,
+    PartitionKey: 'somekey',
+    StreamName: streamName
+  }
+  kinesis.putRecord(record, function (err, res) {
+    if (err) {
+      console.error(err)
+    }
+    else {
+      console.log('Message succesfully sent to the stream:')
+      console.log(data)
+      console.log(res)
+    }
+  })
+}
+
+router.post('/', function (req, res, next) {
+  if (!req.is('application/json')) {
+    next(createError(406))
+  }
+  witeToKinesis(STREAM_NAME, JSON.stringify(req.body))
+  res.json({message: 'ok'})
+})
+
+module.exports = router
